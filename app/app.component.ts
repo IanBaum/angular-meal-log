09e786573ca769996aa3b12d1272603cd76528eb
Ian Baum <ian@ianbaum.com>
2016-10-21 11:26:35 -0400

    add files to edit meals

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
   <div class="container">
     <h1>Meal Log</h1>
-    <h3 *ngFor="let currentMeal of meals">{{ currentMeal.food }}</h3>
+    <meal-list
+    [childMealList]="masterMealList"
+    (clickSender)="showDetails($event)"
+    ></meal-list>
+    <edit-meal
+    [childSelectedMeal]="selectedMeal"
+    (doneClickedSender)="finishedEditing()"
+    ></edit-meal>
   </div>
   `
 })
 
 export class AppComponent {
-  public meals: Meal[] = [
-    new Meal("Toast",0),
-    new Meal("Pasta",1),
-    new Meal("Ice Cream",2)
-  ];
-}
+    public masterMealList: Meal[] = [
+      new Meal("Toast",0),
+      new Meal("Pasta",1),
+      new Meal("Ice Cream",2)
+    ];
 
-export class Meal {
-  constructor(public food: string, public id: number) {   }
+  selectedMeal: Meal = null;
+  showDetails(clickedMeal: Meal){
+    this.selectedMeal = clickedMeal;
+  }
+  finishedEditing() {
+    this.selectedMeal = null;
+  }
 }
---
+++ app/meal.model.ts
@@ -0,0 +1,3 @@
+export class Meal {
+  constructor(public food: string, public id: number) {}
+}
